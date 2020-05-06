cf8d6ef909e0f2306714e3c70b5c899b171e230c
Ricky <etoricky@gmail.com>
2020-05-06 14:52:47 +0800

    better name

--- 03Better/Source.cpp
+++ 03Better/Source.cpp
@@ -3,41 +3,41 @@
 
 class Welford {
 public:
-	void add(double x) {
-		n++;
-		const double new_mean = m + (x - m) / n;
-		const double new_sum = s + (x - m) * (x - new_mean);
-		m = new_mean;
-		s = new_sum;
+	void Add(double x) {
+		count++;
+		const double new_mean = mean + (x - mean) / count;
+		const double new_sum = sum + (x - mean) * (x - new_mean);
+		mean = new_mean;
+		sum = new_sum;
 	}
-	int count() const {
-		return n;
+	int Count() const {
+		return count;
 	}
-	double mean() const {
-		return (n > 0) ? m : 0.0;
+	double Mean() const {
+		return (count > 0) ? mean : 0.0;
 	}
-	double var() const {
-		return ((n > 1) ? s / (n - 1) : 0.0);
+	double Var() const {
+		return ((count > 1) ? sum / (count - 1) : 0.0);
 	}
-	double std() const {
-		return sqrt(var());
+	double Std() const {
+		return sqrt(Var());
 	}
 private:
-	int n = 0;
-	double m = 0, s = 0;
+	int count = 0;
+	double mean = 0, sum = 0;
 };
 
 void main() {
 	Welford object;
-	object.add(3);
-	assert(object.mean() == 3); // =AVERAGE(3)
-	assert(object.var() == 0); // =VAR.S(3)
-	object.add(7);
-	assert(object.mean() == 5); // =AVERAGE(3,7)
-	assert(object.var() == 8); // =VAR.S(3,7)
-	object.add(11);
-	assert(object.mean() == 7); // =AVERAGE(3,7,11)
-	assert(object.var() == 16); // =VAR.S(3,7,11)
+	object.Add(3);
+	assert(object.Mean() == 3); // =AVERAGE(3)
+	assert(object.Var() == 0); // =VAR.S(3)
+	object.Add(7);
+	assert(object.Mean() == 5); // =AVERAGE(3,7)
+	assert(object.Var() == 8); // =VAR.S(3,7)
+	object.Add(11);
+	assert(object.Mean() == 7); // =AVERAGE(3,7,11)
+	assert(object.Var() == 16); // =VAR.S(3,7,11)
 	std::cout << "ok";
 	std::cin.get();
 }
